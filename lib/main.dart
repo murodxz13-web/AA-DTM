ce24ec1743126f1c977b0c38684fabdb10c5c0ad
murodxz13-web <murodxz13@gmail.com>
2026-04-06 23:44:58 +0500

    Create main.dart file for AA-DTM Flutter app entry point

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AA-DTM Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: Text('Welcome to AA-DTM'),
+      ),
+    );
+  }
+}
